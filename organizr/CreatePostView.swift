6f444e75452e5289114f2e334186ce10576d5925
cvxluo <cvxluo@users.noreply.github.com>
2018-04-15 00:36:07 -0400

    making posts is good

--- organizr/CreatePostView.swift
+++ organizr/CreatePostView.swift
@@ -29,60 +29,20 @@ class CreatePostView: UIViewController, UITextFieldDelegate {
         
         let user = Auth.auth().currentUser!
         let userUID = user.uid
-        /*
-        db.collection("users").document(userUID).getDocument { (document, error) in
-            if let document = document, document.exists {
-                let dataDescription = document.data()
-                let gotSchool = dataDescription["school"] as! String
-                var groupsAMemberOf = dataDescription["groups"] as! [String]
-                groupsAMemberOf.append(self.groupName.text!)
-                
-                db.collection("users").document(userUID).updateData([
-                    "groups": groupsAMemberOf
-                ]) { err in
-                    if let err = err {
-                        print("Error updating document: \(err)")
-                    } else {
-                        print("Document successfully updated")
-                    }
-                }
-                
-                
-                db.collection("schools").document(gotSchool).getDocument { (document, error) in
-                    if let document = document, document.exists {
-                        let dataDescription = document.data()
-                        print(dataDescription)
-                        var gotClubs = dataDescription["clubs"] as! [String]
-                        print("howdyyalllll", gotClubs)
-                        
-                        gotClubs.append(self.groupName.text!)
-                        print("why is it all fucked up", gotClubs)
-                        db.collection("schools").document(gotSchool).updateData([
-                            "clubs": gotClubs
-                        ]) { err in
-                            if let err = err {
-                                print("Error updating document: \(err)")
-                            } else {
-                                print("Document successfully updated")
-                            }
-                        }
-                    }
-                }
+        
+    db.collection("groups").document(selectedGroup).collection("posts").document(String(currentDateTime.timeIntervalSinceReferenceDate)).setData([
+            "name" : postName.text!,
+            "description" : postDescription.text!,
+            "creator" : userUID,
+            "timestamp" : String(currentDateTime.timeIntervalSinceReferenceDate)
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added")
             }
         }
         
-        db.collection("groups").document(self.groupName.text!).setData([
-            "name": self.groupName.text!,
-            "description": self.groupDescription.text!,
-            "members": [userUID]
-        ]) { err in
-            if let err = err {
-                print("Error writing document: \(err)")
-            } else {
-                print("Document successfully written!")
-            }
-        }
-        */
         
         
         self.presentingViewController?.dismiss(animated: true, completion: nil)
